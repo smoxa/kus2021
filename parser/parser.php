c48c93128885352718ca7e41bd8180e709385337
smoxa <ssmoxa@gmail.com>
2023-01-10 02:34:58 +0300

    Update and rename parser.txt to parser.php

---
+++ parser/parser.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include 'kus/kus.php';
+    ?>
+</body>
+</html>
